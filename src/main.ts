89979e5bc1332c0d237461e39c296835585ae8af
jeffshell <875899952@qq.com>
2021-05-24 17:21:41 +0800

    创建Doc页面

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createRouter, createWebHashHistory} from "vue-router";
 import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue"
 
 const history = createWebHashHistory()
 const router = createRouter({
     history:history,
     routes: [
-        {path: '/', component: Home}
+        {path: '/', component: Home},
+        {path: '/doc', component: Doc}
+
     ]
 })
 const app = createApp(App)

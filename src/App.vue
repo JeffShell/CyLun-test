979312f7af2e4640a270c2d3d57a6fd5f7c3fa63
jeffshell <875899952@qq.com>
2021-05-24 21:04:28 +0800

    切换路由后关闭侧边栏

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@
 
 <script lang="ts">
 import {ref, provide} from 'vue'
+import {router} from "./router";
 export default {
   setup(){
     const width = document.documentElement.clienWidth;
     const menuVisible  = ref(width <= 500 ? false : true);
-    provide('menuVisible', menuVisible )
+    provide('menuVisible', menuVisible );
+    router.afterEach(()=>{
+      if(width <= 500){
+        menuVisible.value = false;
+      }
+    })
   }
 }
 </script>

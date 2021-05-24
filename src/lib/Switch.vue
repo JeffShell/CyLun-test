1ad82b666a5b229c5a8bf5e23649c46c0027e5ce
jeffshell <875899952@qq.com>
2021-05-25 00:02:15 +0800

    优化css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -3,49 +3,60 @@
   <button @click="toggle" :class="{checked:value}"> <span></span> </button>
 </template>
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
-  props:{
-    value: Boolean
+  props: {
+    value: Boolean,
   },
   setup(props, context) {
     const toggle = () => {
-      context.emit('input', !props.value)
-    }
-    return { toggle}
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @use "sass:math";
 $h: 22px;
 $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: gray;
-    border-radius: math.div($h,2);
-    position: relative;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: math.div($h, 2);
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: math.div($h, 2);
+  transition: all 250ms;
+}
+button.checked {
+  background: #1890ff;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
   }
-  span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: math.div($h,2);
-    transition: left 250ms;
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
   }
-  button.checked{
-    background: blue;
-  }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-  button:focus {
-    outline: none;
-  }
+}
 </style>

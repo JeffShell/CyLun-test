3e4b92c1be1a7158ee0beadc40e5ca995c0cd42b
jeffshell <875899952@qq.com>
2021-05-24 17:18:25 +0800

    创建首页并封装topnav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    home
+  <div class="topnav">
+    <Topnav/>
+    <div class="banner">
+      <h1>纯风UI</h1>
+      <h2>十分方便的UI框架</h2>
+      <p class="actions">
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
+    </div>
   </div>
 </template>
-
-<script>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
 export default {
-  name: "Home.vue"
+  components: {Topnav}
 }
 </script>
+<style lang="scss" scoped>
 
-<style scoped>
 
 </style>

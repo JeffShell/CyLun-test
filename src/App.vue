089ae91f0034beb586549e89f34591c7e99be78a
jeffshell <875899952@qq.com>
2021-05-24 17:09:14 +0800

    安装router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './views/Home.vue'
 export default {
   name: 'App',
   components: {
+    Home,
     HelloWorld
   }
 }

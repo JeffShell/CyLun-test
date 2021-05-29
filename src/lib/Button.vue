afba42ae585a1a14665c14acb82d20a4d0e8b4f5
jeffshell <875899952@qq.com>
2021-05-29 10:02:13 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -14,40 +14,58 @@ export default {
 };
 </script>
 <style lang="scss">
-  div{
-    $h: 32px;
-    $border-color: #d9d9d9;
-    $color: #333;
-    $blue: #40a9ff;
-    $radius: 4px;
-    .cy-button {
-      box-sizing: border-box;
-      height: $h;
-      padding: 0 12px;
-      cursor: pointer;
-      display: inline-flex;
-      justify-content: center;
-      align-items: center;
-      white-space: nowrap;
-      background: white;
-      color: $color;
-      border: 1px solid $border-color;
-      border-radius: $radius;
-      box-shadow: 0 1px 0 fade-out(black, 0.95);
-      & {
-        margin-left: 8px;
-      }
-      &:hover,
-      &:focus {
-        color: $blue;
-        border-color: $blue;
-      }
-      &:focus {
-        outline: none;
-      }
-      &::-moz-focus-inner {
-        border: 0;
-      }
+div {
+  $h: 32px;
+  $border-color: #d9d9d9;
+  $color: #333;
+  $blue: #40a9ff;
+  $radius: 4px;
+  .cy-button {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+    & {
+      margin-left: 8px;
+    }
+    &:hover,
+    &:focus {
+      color: $blue;
+      border-color: $blue;
+    }
+    &:focus {
+      outline: none;
+    }
+    &::-moz-focus-inner {
+      border: 0;
     }
   }
+  &.cy-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.cy-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+}
 </style>

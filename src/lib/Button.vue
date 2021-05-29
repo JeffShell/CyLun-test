5dd5658442f1d470cc83c191b4465d39264e21f2
jeffshell <875899952@qq.com>
2021-05-29 10:48:43 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,13 +15,18 @@ export default {
       type: String,
       default: "normal",
     },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props) {
-    const { theme, size } = props;
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`cy-theme-${theme}`]: theme,
         [`cy-size-${size}`]: size,
+        [`cy-level-${level}`]: level,
       };
     });
     return { classes };
@@ -34,6 +39,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .cy-button {
   box-sizing: border-box;
   height: $h;
@@ -89,6 +95,53 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.cy-theme-button {
+    &.cy-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.cy-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.cy-theme-link {
+    &.cy-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.cy-theme-text {
+    &.cy-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.cy-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>

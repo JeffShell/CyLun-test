dc2949febedbff16d627fd63c3aba919167de3a4
jeffshell <875899952@qq.com>
2021-05-30 16:59:45 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="cy-tabs">
-    <div class="cy-tabs-nav">
+    <div class="cy-tabs-nav" ref="container">
       <div class="cy-tabs-nav-item"
            v-for="(t,index) in titles" @click="select(t)"
+           :ref="el => {if(el) navItems[index] = el}"
            :class="{selected: t=== selected}"
            :key="index">
         {{t}}
       </div>
-      <div class="cy-tabs-nav-indicator"></div>
+      <div class="cy-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="cy-tabs-content">
       <component class="cy-tabs-content-item"
@@ -20,7 +21,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import {computed} from "vue";
+import {computed, onMounted, onUpdated, ref} from "vue";
 export default {
   props:{
     selected: {
@@ -28,6 +29,25 @@ export default {
     }
   },
   setup (props, context) {
+    const navItems = ref <HTMLDivElement[]>([])
+    const indicator = ref <HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = ()=> {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + "px"
+      const {
+        left: left1
+      } = container.value.getBoundingClientRect()
+      const {
+        left: left2
+      } = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -46,7 +66,7 @@ export default {
       context.emit('update:selected', title)
     }
     return {
-      defaults, titles, current, select
+      defaults, titles, current, select, navItems, indicator, container
     }
   }
 }
@@ -80,6 +100,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 50px;
+      transition: all 250ms;
     }
   }
   &-content {

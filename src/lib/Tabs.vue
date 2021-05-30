9b2efbc5d864117be0c825d5a72d0f63157052fa
jeffshell <875899952@qq.com>
2021-05-30 16:45:09 +0800

    添加下划线

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -7,6 +7,7 @@
            :key="index">
         {{t}}
       </div>
+      <div class="cy-tabs-nav-indicator"></div>
     </div>
     <div class="cy-tabs-content">
       <component class="cy-tabs-content-item"
@@ -60,6 +61,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -70,6 +72,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 50px;
     }
   }
   &-content {

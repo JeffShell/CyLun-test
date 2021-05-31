34dd6992cfc61500947400d9b7f0665d10e41350
jeffshell <875899952@qq.com>
2021-05-31 12:44:29 +0800

    icon布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,15 +16,29 @@
     </div>
   </div>
   <div class="features">
-    <svg class="icon">
-      <use xlink:href="#icon-vue"></use>
-    </svg>
-    <svg class="icon">
-      <use xlink:href="#icon-ts"></use>
-    </svg>
-    <svg class="icon">
-      <use xlink:href="#icon-light"></use>
-    </svg>
+    <ul>
+      <li>
+        <svg>
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript </h3>
+        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>骄傲地使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
     </div>
   </div>
 </template>
@@ -38,6 +52,53 @@ export default {
 $green: #02bcb0;
 $border-radius: 4px;
 $color: #007974;
+
+.features {
+  margin: 64px auto;
+  width: 370px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content:space-around;
+    margin-top: 450px;
+    > li {
+      box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+      border-radius: 3px;
+      padding: 10px;
+      width: 370px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text
+      }
+    }
+  }
+}
 .topnav{
   position: relative;
   .bgc {
@@ -57,12 +118,7 @@ $color: #007974;
     border-color: transparent transparent transparent #fff;
     transform: translate(0%, 1%);
   }
-  .features {
-    >svg {
-      width: 64px;
-      height: 64px;
-    }
-  }
+
   .banner {
     color: $color;
     min-width: 300px;
@@ -85,7 +141,6 @@ $color: #007974;
         $h: 28px;
         height: $h;
         line-height: $h;
-        border-radius: $h/5;
         padding: 0 24px;
         border-radius: $border-radius;
         &:hover {
